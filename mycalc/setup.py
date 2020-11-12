925fccb9b4efda4d9e05927ac3c78790477f2561
Glicher-wp <zevs12395@gmail.com>
2020-11-12 21:51:50 +0300

    Add setup

---
+++ mycalc/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="mycalc",
+    version="0.0.1",
+    description="A small example package whith some arifmetics",
+    packages=find_packages(),
+    include_package_data=True,
+    author="It's me, Mario!",
+    author_email="terisht285@gmail.com",
+    url="https://github.com/Glicher-wp/first-package",
+)
